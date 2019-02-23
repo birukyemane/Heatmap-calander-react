cc16a632c87744628d694ed8d214b4656e7cca43
birukyemane <bir_yem@yahoo.com>
2019-02-23 22:32:38 +0200

    make it responsive and improve some styles

--- src/App.js
+++ src/App.js
@@ -14,13 +14,23 @@ class App extends Component {
   
     return (
      <div className="main-wrapper">
-       <header>Heatmap caledar</header>
+       <header>
+         <h1>Heatmap calendar</h1>
+         </header>
        <div className="heatmap">
+        <article>
+          <h2>IBM - 57 Year Stock Price History</h2>
+          <p>Historical daily share price data for IBM since 1962 based on closing data. You can find further info
+            about the data from <a href="https://www.macrotrends.net/stocks/charts/IBM/ibm/stock-price-history">here</a>.
+          </p>
+        </article>       
          <CalendarHeatmap
             startDate={this.state.startDate}
             endDate={this.state.endDate}
             values={this.state.data}
             classForValue={generateClass}
+            showWeekdayLabels={true}
+            tooltipDataAttrs={getToolTip}
           />
        </div>
        <footer>copyright@neuroeventLabs</footer>
@@ -31,7 +41,7 @@ class App extends Component {
 
 export default App;
 
-const generateClass = (value) => {
+const generateClass = value => {
   if (!value) {
     return 'color-empty';
   } else if(value.close < 120){
@@ -51,4 +61,18 @@ const generateClass = (value) => {
   }else if(value.close < 200){
     return 'color-scale-200';
   }
+}
+
+const getToolTip = value => {
+  if (!value.date) {
+    console.log('no value')
+    return {
+      'data-tip': 'data',
+    };
+  } else {
+    console.log('error in value ', value)
+    return {
+      'data-tip': `${value.date.toString().slice(0, 10)} closing price: ${value.close}`,
+    };
+  } 
 }
