0b2b1ddbcf45e940b77153ec5e5cd7f642bf9ad1
birukyemane <bir_yem@yahoo.com>
2019-02-23 14:55:14 +0200

    heatmap component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import CalendarHeatmap from 'react-calendar-heatmap';
 
 class App extends Component {
   render() {
   
     return (
-     <div>
+     <div className="main-wrapper">
        <header>Heatmap caledar</header>
-       <div>
-         <h3>heatmap clarendar goes here</h3>
+       <div className="heatmap">
+         <h3>heatmap clarendar demo</h3>
+         <CalendarHeatmap
+            startDate={new Date('2016-01-01')}
+            endDate={new Date('2016-04-01')}
+            values={[
+              { date: '2016-01-01' },
+              { date: '2016-01-22' },
+              { date: '2016-01-30' },
+              // ...and so on
+            ]}
+          />
        </div>
        <footer>copyright@neuroeventLabs</footer>
      </div>
